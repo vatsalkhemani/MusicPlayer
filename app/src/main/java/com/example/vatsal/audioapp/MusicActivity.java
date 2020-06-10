aa35c2611ef6b92575004c8c72e71007e325091f
Vatsal Khemani <43574951+vatsalkhemani@users.noreply.github.com>
2020-06-10 19:05:09 +0530

    right swipe working

--- app/src/main/java/com/example/vatsal/audioapp/MusicActivity.java
+++ app/src/main/java/com/example/vatsal/audioapp/MusicActivity.java
@@ -13,8 +13,12 @@ import android.view.View.OnTouchListener;
 import android.widget.ImageView;
 import com.example.vatsal.audioapp.OnSwipeTouchListener;
 
-public class MusicActivity extends AppCompatActivity {
+public class MusicActivity extends AppCompatActivity implements GestureDetector.OnGestureListener {
 
+    private static final String TAG="Swipe Position";
+    private float x1,x2,y1,y2;
+    private static int MIN_Distance=150;
+    private GestureDetector gestureDetector;
     MediaPlayer mediaPlayer;
 
     @Override
@@ -24,11 +28,56 @@ public class MusicActivity extends AppCompatActivity {
         mediaPlayer=MediaPlayer.create(MusicActivity.this,R.raw.musicdance);
         mediaPlayer.start();
 
+        this.gestureDetector=new GestureDetector(MusicActivity.this,this);
+
+
     }
 
 
 
+    //overrideontouch
 
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        gestureDetector.onTouchEvent(event);
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                x1=event.getX();
+                y1=event.getY();
+                break;
+
+            case MotionEvent.ACTION_UP:
+                x2=event.getX();
+                y2=event.getY();
+
+                //gettingvalueforhorswipe
+                float valueX=x2-x1;
+                //gettingvalueforverswipe
+                float valueY=y2-y1;
+
+                if(Math.abs(valueX)>MIN_Distance)
+                {
+                    //lefttoright
+                    if(x1>x2)
+                    {
+                        onBackPressed();
+                    }else{
+
+                    }
+                }else{
+
+                }
+
+
+
+
+        }
+
+
+        return super.onTouchEvent(event);
+    }
 
     @Override
     public void onBackPressed() {
@@ -54,4 +103,33 @@ public class MusicActivity extends AppCompatActivity {
     }
 
 
+    @Override
+    public boolean onDown(MotionEvent motionEvent) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent motionEvent) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent motionEvent) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent motionEvent) {
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+        return false;
+    }
 }
